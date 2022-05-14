2fb1ca31d358f564ab8ff57d4e6a664f9e4ded18
Alexey Leonov <x1leon1x@gmail.com>
2022-05-14 21:01:57 +0300

    добавил cards

---
+++ Mystore/Controllers/CardsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mystore.Controllers
+{
+    public class CardsController : Controller
+    {
+        [Route("cards/{id}")]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
